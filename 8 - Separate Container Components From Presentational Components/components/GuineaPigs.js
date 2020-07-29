baf10861bd7a7e9d9d80386e38cc3ca62651485e
Sarah ALIBAY <alibaymand@eisti.eu>
2020-07-29 22:22:52 +0200

    add stateless components and props

--- 8 - Separate Container Components From Presentational Components/components/GuineaPigs.js
+++ 8 - Separate Container Components From Presentational Components/components/GuineaPigs.js
@@ -11,3 +11,17 @@ export class GuineaPigs extends React.Component {
     );
   }
 }
+
+
+// Stateless version voir 9 
+import React from 'react';
+
+export const GuineaPigs = (props) => {
+  let src = props.src;
+  return (
+    <div>
+      <h1>Cute Guinea Pigs</h1>
+      <img src={src} />
+    </div>
+  );
+}
--- 9 - Stateless Functional Components/Example.js
+++ 9 - Stateless Functional Components/Example.js
@@ -15,3 +15,36 @@ export class MyComponentClass extends React.Component {
       <MyComponentClass />,
       document.getElementById('app')
   );
+
+
+
+
+
+
+  // Normal way to display a prop:
+export class MyComponentClass extends React.Component {
+  render() {
+    return <h1>{this.props.title}</h1>;
+  }
+}
+
+// Stateless functional component way to display a prop:
+export const MyComponentClass = (props) => {
+  return <h1>{props.title}</h1>;
+}
+
+// Normal way to display a prop using a variable:
+export class MyComponentClass extends React.component {
+  render() {
+  	let title = this.props.title;
+    return <h1>{title}</h1>;
+  }
+}
+
+// Stateless functional component way to display a prop using a variable:
+export const MyComponentClass = (props) => {
+	let title = props.title;
+  return <h1>{title}</h1>;
+}
+
+// Voir 8 - GuineaPigs.js pour version stateless
